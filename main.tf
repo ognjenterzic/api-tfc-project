37b690eb5ab2e29d21d45fb52415aacfa4b42841
Ognjen Terzic <Terzic.Ognjen@hotmail.com>
2023-01-11 10:12:20 +0100

    Testing first resource deployment by api driven workflow

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws={
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+
+ #####CLOUD 
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "ogi-bucket" {
+  bucket = "ogi-bucket-s3"
+  force_destroy = true
+  acl = "public-read"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
